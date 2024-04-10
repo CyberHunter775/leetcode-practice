4f5f3e423142c3e9a937e71f63d9c303e7ce3e01
hejingxin <hejx@gih.cn>
2024-04-10 23:30:24 +0800

    xxx

---
+++ src/main/java/com/gnf/leetcode/hot100/hash/TMainHash.java
@@ -0,0 +1,13 @@
+package com.gnf.leetcode.hot100.hash;
+
+/**
+ * @author hejingxin
+ * @date 2024-04-10 23:17
+ */
+public class TMainHash {
+
+
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
